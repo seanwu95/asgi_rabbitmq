88ec6eda2d7e8ae97a871df9a6275fd2f08eddfc
Artem Malyshev <proofit404@gmail.com>
2016-12-06 11:54:20 +0300

    Add minimal setup.py spec.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='asgi_rabbitmq',
+    version='0.1.dev1',
+    description='RabbitMQ backend for ASGI',
+    url='https://github.com/proofit404/asgi_rabbitmq',
+    license='BSD',
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    maintainer='Artem Malyshev',
+    maintainer_email='proofit404@gmail.com',
+    packages=find_packages())
