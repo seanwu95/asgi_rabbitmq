3262870156db2c92faf3c76261e056cb94738368
Artem Malyshev <proofit404@gmail.com>
2016-12-16 02:38:02 +0300

    Initiate parent channel layer.

--- asgi_rabbitmq/core.py
+++ asgi_rabbitmq/core.py
@@ -3,6 +3,18 @@ from asgiref.base_layer import BaseChannelLayer
 
 class RabbitmqChannelLayer(BaseChannelLayer):
 
-    def __init__(self, *args, **kwargs):
+    extensions = ['groups', 'twisted']
 
-        pass
+    def __init__(self,
+                 expiry=60,
+                 group_expiry=86400,
+                 capacity=100,
+                 channel_capacity=None,
+                 **kwargs):
+
+        super(RabbitmqChannelLayer, self).__init__(
+            expiry=expiry,
+            group_expiry=group_expiry,
+            capacity=capacity,
+            channel_capacity=channel_capacity,
+        )
