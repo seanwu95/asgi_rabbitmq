88ebbd76297c21081383ba3435f43dd492cf208a
Artem Malyshev <proofit404@gmail.com>
2016-12-16 04:41:40 +0300

    Declare necessary methods.

--- asgi_rabbitmq/core.py
+++ asgi_rabbitmq/core.py
@@ -1,4 +1,5 @@
 from asgiref.base_layer import BaseChannelLayer
+from pika import BlockingConnection, URLParameters
 
 
 class RabbitmqChannelLayer(BaseChannelLayer):
@@ -6,11 +7,11 @@ class RabbitmqChannelLayer(BaseChannelLayer):
     extensions = ['groups', 'twisted']
 
     def __init__(self,
+                 url,
                  expiry=60,
                  group_expiry=86400,
                  capacity=100,
-                 channel_capacity=None,
-                 **kwargs):
+                 channel_capacity=None):
 
         super(RabbitmqChannelLayer, self).__init__(
             expiry=expiry,
@@ -18,3 +19,33 @@ class RabbitmqChannelLayer(BaseChannelLayer):
             capacity=capacity,
             channel_capacity=channel_capacity,
         )
+        parameters = URLParameters(url)
+        self.connection = BlockingConnection(parameters)
+
+    def send(self, channel, message):
+
+        pass
+
+    def receive(self, channels, block=False):
+
+        return None, None
+
+    def new_channel(self, pattern):
+
+        pass
+
+    def group_add(self, group, channel):
+
+        pass
+
+    def group_discard(self, group, channel):
+
+        pass
+
+    def group_channels(self, group):
+
+        return []
+
+    def send_group(self, group, message):
+
+        pass
