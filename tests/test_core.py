246cda8c797cc9dad928771419e2aca39cd8ec62
Artem Malyshev <proofit404@gmail.com>
2016-12-21 08:22:31 +0300

    Set low group_expiry value for tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,7 +8,8 @@ class RabbitmqChannelLayerTest(ConformanceTestCase):
     @pytest.fixture(autouse=True)
     def init_conformance_test(self, vhost):
 
-        self.channel_layer = RabbitmqChannelLayer(vhost, expiry=1, capacity=5)
+        self.channel_layer = RabbitmqChannelLayer(
+            vhost, expiry=1, group_expiry=2, capacity=5)
 
     expiry_delay = 1.1
     capacity_limit = 5
