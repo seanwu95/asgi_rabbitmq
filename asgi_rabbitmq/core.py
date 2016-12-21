a4996359b4e40cc3a2e3a35c56cc4a824cacef37
Artem Malyshev <proofit404@gmail.com>
2016-12-21 11:13:07 +0300

    Implement new_channel.
    Passive declare of the queue checks if it exists.  So we can decide if
    generated channel name is unique at this moment.
    
    Implement re-consume if we have non existing queues in the receive
    arguments.

--- asgi_rabbitmq/core.py
+++ asgi_rabbitmq/core.py
@@ -1,7 +1,10 @@
+import random
+import string
 from functools import partial
 
 from asgiref.base_layer import BaseChannelLayer
 from pika import BlockingConnection, URLParameters
+from pika.exceptions import ChannelClosed
 from pika.spec import BasicProperties
 
 
@@ -28,6 +31,8 @@ class RabbitmqChannelLayer(BaseChannelLayer):
 
     def send(self, channel, message):
 
+        if not self.amqp_channel.is_open:  # FIXME: duplication :(
+            self.amqp_channel = self.amqp_connection.channel()
         expiration = str(self.expiry * 1000)
         properties = BasicProperties(headers=message, expiration=expiration)
         reply = self.amqp_channel.queue_declare(queue=channel)
@@ -42,13 +47,34 @@ class RabbitmqChannelLayer(BaseChannelLayer):
 
     def receive(self, channels, block=False):
 
+        if not self.amqp_channel.is_open:  # FIXME: duplication :(
+            self.amqp_channel = self.amqp_connection.channel()
         result = Result()
+        callbacks = {
+            channel_name: partial(self._on_message, result, channel_name)
+            for channel_name in channels
+        }
 
         # FIXME: don't consume on queues from previous `receive`
         # call.  Clean up `amqp_channel` state.
-        for channel_name in channels:
-            on_message = partial(self._on_message, result, channel_name)
-            self.amqp_channel.basic_consume(on_message, queue=channel_name)
+        #
+        # FIXME: block if all queues doesn't exists
+        consumed = False
+        while not consumed:
+            for channel_name, callback in callbacks.items():
+                try:
+                    self.amqp_channel.basic_consume(
+                        callback, queue=channel_name)
+                except ChannelClosed as e:
+                    if not_found_error(e):
+                        del callbacks[channel_name]
+                        # FIXME: duplication :(
+                        self.amqp_channel = self.amqp_connection.channel()
+                        break
+                    else:
+                        raise
+            else:
+                consumed = True
 
         time_limit = None if block else 0
         self.amqp_connection.process_data_events(time_limit)
@@ -62,7 +88,21 @@ class RabbitmqChannelLayer(BaseChannelLayer):
 
     def new_channel(self, pattern):
 
-        pass
+        assert pattern.endswith('!') or pattern.endswith('?')
+
+        while True:
+            chars = (random.choice(string.ascii_letters) for _ in range(12))
+            random_string = ''.join(chars)
+            channel = pattern + random_string
+            try:
+                self.amqp_channel.queue_declare(queue=channel, passive=True)
+            except ChannelClosed as e:
+                # FIXME: Channel is always closed on next retry.  Open
+                # new one here.
+                if not_found_error(e):
+                    return channel
+                else:
+                    raise
 
     def group_add(self, group, channel):
 
@@ -103,3 +143,8 @@ class Result(object):
     def __bool__(self):
 
         return self.value is not None
+
+
+def not_found_error(exception):
+
+    return exception.args[0] == 404
