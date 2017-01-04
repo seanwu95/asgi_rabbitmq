1bf946c50d6522f5db4078c9feb009b44ab4343a
Artem Malyshev <proofit404@gmail.com>
2017-01-04 05:23:24 +0300

    Replace hand written test websocket client with third party package.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 pytest_plugins = [
     'management_fixtures',
-    'ws_client_fixtures',
     'asgi_server_fixtures',
 ]
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,21 @@
-def test_websocket_message(ws_client):
+import requests
+import websocket
+
+
+def test_http_request(asgi_server):
+    """Test the ability to send http requests and receive responses."""
+
+    url = 'http://%s:%d/' % asgi_server
+    response = requests.get(url)
+    assert response.status_code == 404
+
+
+def test_websocket_message(asgi_server):
     """Test the ability to send and receive messages over WebSocket."""
 
-    message = 'test'.encode()
-    response = ws_client(message)
-    assert message == response
+    url = 'ws://%s:%d/' % asgi_server
+    ws = websocket.create_connection(url)
+    ws.send('test')
+    response = ws.recv()
+    ws.close()
+    assert 'test' == response
