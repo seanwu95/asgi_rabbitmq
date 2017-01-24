be756707abd397b0e6b6f7333a166d2ee0ccc178
Artem Malyshev <proofit404@gmail.com>
2017-01-24 03:05:14 +0300

    Run benchmark as part of py.test suite.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,7 @@
+import benchmark
 import requests
 import websocket
+from twisted.internet import reactor
 
 
 def test_http_request(asgi_server):
@@ -19,3 +21,28 @@ def test_websocket_message(asgi_server):
     response = ws.recv()
     ws.close()
     assert 'test' == response
+
+
+class Benchmarker(benchmark.Benchmarker):
+
+    def print_progress(self):
+
+        print('.', end='', flush=True)
+        open_protocols = len([x for x in benchmark.stats.values() if not x])
+        if open_protocols == 0 and len(benchmark.stats) >= self.num:
+            reactor.stop()
+            self.print_stats()
+
+
+def test_benchmark(asgi_server):
+
+    benchmarker = Benchmarker(
+        url='ws://%s:%d' % asgi_server,
+        num=100,
+        concurrency=10,
+        rate=1,
+        messages=5,
+        spawn=30,
+    )
+    benchmarker.loop()
+    reactor.run()
