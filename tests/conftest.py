68876be25c8d6cd76b7eec554e305c70163fc5f0
Artem Malyshev <proofit404@gmail.com>
2017-01-26 21:38:28 +0300

    Use multiprocessing spawn method.
    This will prevent artifacts from previous test run affects fixtures
    for current test.  For example twisted reactor state is global.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import multiprocessing
+
+try:
+    multiprocessing.set_start_method('spawn')
+except AttributeError:
+    pass
+
 pytest_plugins = [
     'management_fixtures',
     'asgi_server_fixtures',
