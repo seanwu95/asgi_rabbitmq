97fc3b49cc8d0558e056c2e76ebb1e715261b5e9
Artem Malyshev <proofit404@gmail.com>
2017-01-28 01:47:28 +0300

    Run benchmark inside child process.
    This allows to start twisted reactor twice and so to run benchmark
    twice as well.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+import multiprocessing
 
 import benchmark
 import requests
@@ -25,24 +25,24 @@ def test_websocket_message(asgi_server):
     assert 'test' == response
 
 
-# asgi_server is a parametized fixture, but twisted reactor was
-# designed to run only ones.  This is the dirty hack to make
-# test_benchmark function idempotent.
-run_benchmark = True
-
-
 def test_benchmark(asgi_server):
 
-    global run_benchmark
-    if run_benchmark:
-        benchmarker = benchmark.Benchmarker(
-            url='ws://%s:%d' % asgi_server,
-            num=100,
-            concurrency=10,
-            rate=1,
-            messages=5,
-            spawn=30,
-        )
-        benchmarker.loop()
-        reactor.run(installSignalHandlers=False)
-        run_benchmark = False
+    proc = multiprocessing.Process(target=run_benchmark, args=(asgi_server,))
+    proc.daemon = True
+    proc.start()
+    proc.join()
+    assert proc.exitcode == 0
+
+
+def run_benchmark(asgi_server):
+
+    benchmarker = benchmark.Benchmarker(
+        url='ws://%s:%d' % asgi_server,
+        num=100,
+        concurrency=10,
+        rate=1,
+        messages=5,
+        spawn=30,
+    )
+    benchmarker.loop()
+    reactor.run(installSignalHandlers=False)
