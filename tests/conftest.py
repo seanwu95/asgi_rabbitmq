e866cff63a0ad83b6b35694eda275eb9af131b1b
Artem Malyshev <proofit404@gmail.com>
2017-03-06 09:02:13 +0300

    Remove unused fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,25 @@
 import multiprocessing
 
+import amqpstat
+import pytest
+
 try:
     multiprocessing.set_start_method('spawn')
 except AttributeError:
     pass
 
-pytest_plugins = [
-    'asgi_server_fixtures',
-    'benchmark_fixtures',
-    'management_fixtures',
-]
+
+@pytest.fixture(scope='session', autouse=True)
+def statistics(statdir):
+    """Print benchmark statistics table."""
+
+    amqpstat.maybe_monkeypatch()
+    yield
+    amqpstat.maybe_print_stats(statdir)
+
+
+@pytest.fixture(scope='session')
+def statdir(tmpdir_factory):
+
+    d = tmpdir_factory.mktemp('stats', numbered=True)
+    return str(d)
