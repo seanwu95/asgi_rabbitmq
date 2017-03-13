06f7c157c3a601a84e35da406d3b84eac15b0e9d
Artem Malyshev <proofit404@gmail.com>
2017-03-14 02:21:55 +0300

    Restore pytest-django usage.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import multiprocessing
 import os
 import sys
 
-import django
 import pytest
 
 try:
@@ -10,22 +9,12 @@ try:
 except AttributeError:
     pass
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'testproject.settings.channels_rabbitmq',
-)
-
 sys.path.append(
     os.path.join(
         os.path.dirname(os.path.dirname(__file__)),
         'libs',
     ),
 )
-
-if django.VERSION >= (1, 10):
-    django.setup(set_prefix=False)
-else:
-    django.setup()
 
 
 @pytest.fixture(scope='session', autouse=True)
