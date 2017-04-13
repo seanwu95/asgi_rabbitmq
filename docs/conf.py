872019b621fc08da2298ef620039bfb804164ada
Artem Malyshev <proofit404@gmail.com>
2017-04-13 03:21:53 +0300

    Init sphinx docs.

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+extensions = []
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = 'asgi_rabbitmq'
+copyright = '2017, Django Software Foundation'
+author = 'Django Software Foundation'
+
+version = '0.3'
+release = '0.3'
+
+language = None
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = 'sphinx'
+
+todo_include_todos = False
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+htmlhelp_basename = 'asgi_rabbitmqdoc'
+
+latex_elements = {}
+
+latex_documents = [
+    (master_doc, 'asgi_rabbitmq.tex', 'asgi\\_rabbitmq Documentation',
+     'Django Software Foundation', 'manual'),
+]
+
+man_pages = [
+    (master_doc, 'asgi_rabbitmq', 'asgi_rabbitmq Documentation',
+     [author], 1),
+]
+
+texinfo_documents = [
+    (master_doc, 'asgi_rabbitmq', 'asgi_rabbitmq Documentation', author,
+     'asgi_rabbitmq', 'One line description of project.', 'Miscellaneous'),
+]
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+Welcome to asgi_rabbitmq's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
